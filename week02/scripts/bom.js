dfd5f457a444e5084b25727047052df1e41f7187
c4iro-dev <dan.reyescairo@gmail.com>
2025-11-07 16:54:10 -0800

    Updated JS content  to live inside of the button event listener

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,23 +1,27 @@
 const button = document.querySelector("button");
 const input = document.querySelector('#favchap');
 const list = document.querySelector('#list');
-const li = document.createElement('li');
-const btn_del = document.createElement('button');
 
-li.textContent = input.value;
-btn_del.textContent = "❌";
-li.append(btn_del);
-list.append(li);
-button.setAttribute("aria-label", "Close");
 button.addEventListener('click', function() {
     if (input.value.trim() !== '') {
-        console.log("Nice.");
-    }
-});
+        
+        const li = document.createElement('li');
+        const btn_del = document.createElement('button');
 
-btn_del.addEventListener('click', () => {
-    list.removeChild(li);
-    input.focus();
-    input.value('');
-    input.focus();
+        li.textContent = input.value;
+        btn_del.textContent = "❌";
+        btn_del.setAttribute("aria-label", "Delete Chapter");
+
+        li.append(btn_del);
+        list.append(li);
+
+        btn_del.addEventListener('click', () => {
+            list.removeChild(li);
+            input.value = '';
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+    }
 });
