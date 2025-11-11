7c12f9814f02e9f6bfc8a8c69c0bfb59039b3e02
c4iro-dev <dan.reyescairo@gmail.com>
2025-11-11 15:48:33 -0800

    Functions exercise

---
+++ week03/scripts/prepare-functions.js
@@ -0,0 +1,12 @@
+function fullName(first, last) {
+    return first + " " + last;
+}
+
+const fullName = function (first, last) {
+    return `${first} ${last}`;
+}
+
+const fullName = (first, last) => `${first} ${last}`;
+
+
+document.getElementById("fullName").innerHTML(fullName);
