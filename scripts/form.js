ef91f5ceee8cc411806fb8ee2229bdd321768712
c4iro-dev <dan.reyescairo@gmail.com>
2025-12-06 15:56:13 -0800

    Week05 Assignment - as much as I have atm.

---
+++ scripts/form.js
@@ -0,0 +1,50 @@
+// Product Dropdown
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+
+let product_html = products.map((product) => {
+    return `<option value="${product.id}" >${product.name}</option>`;
+}).join("");
+
+document.querySelector("#product-name").insertAdjacentHTML("beforeend", product_html);
+
+// Review Counter
+let btn_submit = document.querySelector("input[type='submit']");
+btn_submit.addEventListener('submit', function (event) {
+    let numReviews = Number(window.localStorage.getItem('numReviews-ls')) || 0;
+    numReviews++;
+    localStorage.setItem('numReviews-ls', numReviews);
+});
+
+
+// Footer Date Details
+
+const today = new Date();
+document.getElementById("currentYear").innerHTML = `${today.getFullYear()}`;
+document.getElementById("lastModified").innerHTML = "Last modification: " + document.lastModified;
