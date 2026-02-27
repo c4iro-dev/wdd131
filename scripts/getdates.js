1c9f3a2fbff4606c92cb296f8fc354b82bfb0d0f
c4iro-dev <dan.reyescairo@gmail.com>
2026-02-27 10:00:16 -0800

    fix: improperly nested p tag inside span, header levels, missing google font, missing id

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
 const year = document.querySelector("#currentyear");
 const today = new Date();
 year.innerHTML = `${today.getFullYear()}`;
-document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
+document.getElementById("lastModified").innerHTML = `<span class="highlight">Last Modification: ` + document.lastModified + `</span>`;
