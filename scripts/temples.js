8e9f81ce1449dbff634a7b4288cfb1a7cdc379ef
c4iro-dev <dan.reyescairo@gmail.com>
2026-07-11 09:06:07 -0400

    init: temple picture album framework and content, styling, css

---
+++ scripts/temples.js
@@ -0,0 +1,7 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = `${today.getFullYear()}`;
+document.getElementById("lastModified").innerHTML =
+  `<span class="highlight">Last Modification: ` +
+  document.lastModified +
+  `</span>`;
